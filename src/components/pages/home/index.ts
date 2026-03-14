633bf68b85bc70ae6c556296bc3e5ff562d54642
Benill0 <elbenitope@gmail.com>
2026-03-14 04:24:53 -0500

    feat(home): :sparkles: implement mock design home

---
+++ src/components/pages/home/index.ts
@@ -0,0 +1,7 @@
+export { default as CTA } from "./CTA.astro";
+export { default as Hero } from "./Hero.astro";
+export { default as HowItWork } from "./HowItWork.astro";
+export { default as NewServiceTeaser } from "./NewServiceTeaser.astro";
+export { default as PartnerStore } from "./PartnerStore.astro";
+export { default as Services } from "./Services.astro";
+export { default as WhyUs } from "./WhyUs.astro";
