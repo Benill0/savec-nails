c3448596560ecc3e0ce63a1d28db3c49810722cf
Benill0 <elbenitope@gmail.com>
2026-03-14 17:59:25 -0500

    feat(location): :sparkles: add Hero and LocationInfo components with contact details and map integration

--- src/data/contact.ts
+++ src/data/contact.ts
@@ -1,8 +1,14 @@
 export const contactInfo = {
-  phone: "+51 999 999 999",
+  phone: "+51902128282",
   email: "hola@savecnails.com",
-  whatsapp: "https://wa.me/51999999999",
-  googleMaps: "https://goo.gl/maps/...",
+  whatsapp: "https://wa.me/51902128282",
+  googleMaps:
+    "https://www.google.com/maps/place/Savec+Nails/@-12.1986578,-76.9527783,17z/data=!3m1!4b1!4m6!3m5!1s0x9105b98a3bb096c9:0x29015f90a25326d6!8m2!3d-12.1986578!4d-76.9527783!16s%2Fg%2F11n4g079yx!18m1!1e1?entry=ttu&g_ep=EgoyMDI2MDMxMS4wIKXMDSoASAFQAw%3D%3D",
+  coords: {
+    lat: -12.1986578,
+    lng: -76.9527783,
+  },
+  address: "Av. Pacto Andino, Villa El Salvador 15829",
   schedule: [
     { day: "Lun - Vie", hours: "9:00 AM - 8:00 PM" },
     { day: "Sáb", hours: "9:00 AM - 6:00 PM" },
