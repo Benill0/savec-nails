cba61028182b737c08d7a67611a27dae2f46ed6b
Benill0 <elbenitope@gmail.com>
2026-03-14 19:49:05 -0500

    feat(analytics): :sparkles: add Vercel Analytics integration and update configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-
+import sitemap from '@astrojs/sitemap';
 import icon from 'astro-icon';
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [icon({
+  integrations: [sitemap(), icon({
     include: {
       lucide: ['*'],
     },
